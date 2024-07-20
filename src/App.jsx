de03b20ce8097a57656db762ff8f64f56ef9853e
Sudarsan-xc <sudarsansharma2020@gmail.com>
2024-07-20 12:43:01 +0545

    fixed footer in the bottom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import Footer from "./layouts/Footer";
+
 function App() {
-  return <>Hello World</>;
+  return (
+    <>
+      <>
+        <div className="container vh-90">
+          <div
+            className="d-flex justify-content-center"
+            style={{ minHeight: "575px" }}
+          >
+            <Home />
+          </div>
+          <Footer />
+        </div>
+      </>
+    </>
+  );
 }
 
 export default App;
